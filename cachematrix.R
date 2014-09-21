21751489cdf9b8446a06bca543fcc63e9ce6264f
Ho Ji Weng <zirong@outlook.com>
2014-09-21 15:59:19 +0800

    test upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,11 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ##set the matrix to x
+  set <- function( matrix ) {
+    x <<- matrix
+  }
 
 }
 
